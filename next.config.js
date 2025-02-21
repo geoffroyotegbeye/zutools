a31ee0fb917889c2c2f61c65b42c90231c16ae2f
Geoffroy OTEGBEYE <gotegbeye@hyperion-dev.com>
2025-02-21 13:47:20 +0100

    fixbug()

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// module.exports = nextConfig;
+// next.config.js
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
